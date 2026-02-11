791154e4183b29941854913b70e4e2824feb1650
sakshimadne <sakshimadne12345@gmail.com>
2026-02-11 09:40:13 +0530

    bug fixes

--- config/db.js
+++ config/db.js
@@ -1,25 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const connectDB = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGODB_URI);
-//         console.log(`MongoDB Connected: ${conn.connection.host}`);
-
-
-//         try {
-//             await mongoose.connection.db.admin().ping();
-//             console.log('MongoDB Ping Successful: Database is responsive');
-//         } catch (pingError) {
-//             console.error('MongoDB Ping Failed:', pingError);
-//         }
-
-//     } catch (error) {
-//         console.error(`Error: ${error.message}`);
-//         process.exit(1);
-//     }
-// };
-
-// module.exports = connectDB;
 
 
 
@@ -29,9 +8,9 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error('❌ MongoDB connection failed:', error.message);
+        console.error('MongoDB connection failed:', error.message);
         process.exit(1);
     }
 };
